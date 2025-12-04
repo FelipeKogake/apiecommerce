cf35268e44d3829feedc21e74b746bc4b0b738b5
Felipe Kogake <felipe.kogake77@gmail.com>
2025-12-04 18:27:29 -0300

    Segundo commit

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,6 +10,8 @@ import (
 	"apiecommerce2/categoria"
 	mongodbCategoria"apiecommerce2/categoria/mongodb"
 	"github.com/gin-gonic/gin"
+	mongodbUsuario "apiecommerce2/internal/usuario/mongodb"
+	"apiecommerce2/internal/usuario"
 )
 
 func main() {
@@ -21,17 +23,21 @@ func main() {
 	ctx := context.Background()
 	repoProduto, _ := mongodbProduto.NewRepository(cfg.Database.Nome, "produto", cfg.Database.URI)
 	repoCategoria, _ := mongodbCategoria.NewRepository(cfg.Database.Nome, "categoria", cfg.Database.URI)
+	repoUsuario, _ := mongodbUsuario.NewRepository(cfg.Database.Nome, "usuario", cfg.Database.URI)
 	
 	defer repoProduto.Close(ctx)
 	defer repoCategoria.Close(ctx)
+	defer repoUsuario.Close(ctx)
 	
 	serviceProduto := produto.NewService(repoProduto)
 	serviceCategoria := categoria.NewService(repoCategoria)
+	serviceUsuario := usuario.NewService(repoUsuario)
 
 	g := gin.Default()
 
 	internal.RoutesProduto(ctx, g, serviceProduto)
 	internal.RoutesCategoria(ctx, g, serviceCategoria)
+	internal.RoutesUsuario(ctx, g, repoUsuario, serviceUsuario)
 	
 
 	
